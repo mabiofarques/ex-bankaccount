142622c134bd92887d6092bd1d6d77fa60e2ab6e
Fabio Marques <fabio_91marques@hotmail.com>
2025-02-24 10:15:45 -0300

    Estrutura do projeto criada

---
+++ src/model/entities/BankAccount.java
@@ -0,0 +1,34 @@
+package model.entities;
+
+public class BankAccount {
+    private String holder;
+    private String numberAccount;
+    private double balance;
+
+    public BankAccount(){
+    }
+
+    public BankAccount(String holder, String numberAccount, double balance) {
+        this.holder = holder;
+        this.numberAccount = numberAccount;
+        this.balance = balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public String getNumberAccount() {
+        return numberAccount;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
